abad53ea5fa2dd2e59ac56f94354e7c24212557c
javadbat <javadbat@gmail.com>
2025-02-01 15:36:31 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,26 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-select",
+    path: "./lib/index.ts",
+    outputPath: "./dist/index.js",
+    umdName: "JBSelect",
+    external: ["jb-validation"],
+    globals: {
+      "jb-validation": "JBValidation",
+    },
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-select-react",
+    path: "./react/lib/index.tsx",
+    outputPath: "./react/dist/index.js",
+    external: ["jb-select", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
